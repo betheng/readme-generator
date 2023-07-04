6ca36b32209ace9f423bb2f1cff1643dc08020f3
betheng <131614496+betheng@users.noreply.github.com>
2023-07-04 16:01:59 -0400

    update content when user does not use a license

--- utils/readmeQuestions.js
+++ utils/readmeQuestions.js
@@ -37,7 +37,7 @@ const questions = [
       "MIT",
       "Apache",
       "Boost",
-      "NONE - This repo does not have any license associated."
+      "NONE--This_repo_does_not_have_any_license_associated."
     ],
   },
   {
